d7f4baec0b6b31fb91426fb4a9ebf38664189f8b
Yan <Yan@YIHANs-MacBook-Pro.local>
2017-10-03 22:13:25 -0400

    why pointer can't be used for arry but both number and arrary can assign to pointer use & or *

--- address_test.cpp
+++ address_test.cpp
@@ -6,9 +6,17 @@ int main(){
 
 	int arrary1[2] ={4,5};
 
-	int* arrary_pointer1, *arrary_pointer2, *arrary_pointer3;
+	int number=6;
+
+	////////////////////////////////////
+
+	int *arrary_pointer1, *arrary_pointer2, *arrary_pointer3;
 
 	int *arrary_pointer4, *arrary_pointer5;
+
+	int *number_pointer;
+
+	/////////////////////////
 
 	cout << "check check \n";
 
@@ -16,8 +24,14 @@ int main(){
 	arrary_pointer2 = &arrary[1];
 	arrary_pointer3 = &arrary[2];
 
+	cout << "check check \n";
+
 	arrary_pointer4 = &arrary1[0];
-	arrary_pointer5 = &arrary1[1];
+	arrary_pointer5 = &arrary1[0];
+
+	cout << "check check \n";
+
+	*number_pointer = number;
 
 	cout << arrary[0] << "\n";
 	cout << arrary[1] << "\n";
@@ -26,8 +40,11 @@ int main(){
 	cout << arrary_pointer2 << "\n";
 	cout << arrary_pointer3 << "\n";
 
-	cout << *arrary_pointer4 << "\n";
-	cout << *arrary_pointer5 << "\n";
+	cout << arrary_pointer4 << "\n";
+	cout << arrary_pointer5 << "\n";
+
+	cout << number_pointer << "\n";
+
 
 	return 0;
 }
