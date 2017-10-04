7ce65457a2df1ee3f4211f58fc699414b95a8dba
Yan <Yan@YIHANs-MacBook-Pro.local>
2017-10-04 17:56:41 -0400

    change made to address_test, why arrary can only have one pointer point to it?

--- address_test.cpp
+++ address_test.cpp
@@ -8,6 +8,8 @@ int main(){
 
 	int number=6;
 
+	int arrary2[1] = {7};
+
 	////////////////////////////////////
 
 	int *arrary_pointer1, *arrary_pointer2, *arrary_pointer3;
@@ -15,6 +17,8 @@ int main(){
 	int *arrary_pointer4, *arrary_pointer5;
 
 	int *number_pointer;
+
+	int *arrary_pointer6[1];
 
 	/////////////////////////
 
@@ -24,15 +28,6 @@ int main(){
 	arrary_pointer2 = &arrary[1];
 	arrary_pointer3 = &arrary[2];
 
-	cout << "check check \n";
-
-	arrary_pointer4 = &arrary1[0];
-	arrary_pointer5 = &arrary1[0];
-
-	cout << "check check \n";
-
-	*number_pointer = number;
-
 	cout << arrary[0] << "\n";
 	cout << arrary[1] << "\n";
 	cout << arrary[2] << "\n";
@@ -40,11 +35,42 @@ int main(){
 	cout << arrary_pointer2 << "\n";
 	cout << arrary_pointer3 << "\n";
 
+	cout << "check check \n";
+
+	arrary_pointer4 = &arrary1[0];
+	arrary_pointer5 = &arrary1[0];
+
 	cout << arrary_pointer4 << "\n";
 	cout << arrary_pointer5 << "\n";
 
+	cout << "check check \n";
+
+	*number_pointer = number;
+
 	cout << number_pointer << "\n";
 
+	cout << "check check \n";
+
+	*arrary_pointer6 = arrary2;
+
+	cout << arrary_pointer6 << "\n";
+
+	////////////////
+	int array3[2] = {8,9};
+	int *tester1[1];//, *tester2[1];
+	int *tester2[1];
+
+	cout << "check check 3\n";
+
+	*tester1[0] = array3[1];
+	//*tester1[1] = array3[0];
+	*tester2[0] = *tester1[0];
+
+	cout << "check check \n";
+
+	cout << tester1[0] << "\n";
+	cout << tester2[0] << "\n";
+	
 
 	return 0;
 }
